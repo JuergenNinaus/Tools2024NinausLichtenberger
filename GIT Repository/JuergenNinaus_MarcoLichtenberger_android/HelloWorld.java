ea61b416a91c6bb517c652f14a1c96452f49679f
Jürgen Ninaus <S1910458036@students.fh-hagenberg.at>
2024-04-28 22:26:32 +0200

    added LoC to functions

--- GIT Repository/JuergenNinaus_MarcoLichtenberger_android/HelloWorld.java
+++ GIT Repository/JuergenNinaus_MarcoLichtenberger_android/HelloWorld.java
@@ -2,9 +2,11 @@ def main():
     name = "User"
     
     print(f"Hello, {name}! Welcome to this Python program.")
+    print("More Code")
 
 def thisIsANewMethod();
    	print("This is a new method");
+    print("Hi")
 
 if __name__ == "__main__":
     main()
